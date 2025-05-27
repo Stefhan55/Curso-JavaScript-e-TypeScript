a9c468118ec15fb0a403f71346f3c0b16c73b678
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-05-27 19:21:42 -0300

    declaração de strings

---
+++ aula3/index.js
@@ -0,0 +1,8 @@
+console.log("diuliano 'stefhano'");
+console.log('diuliano "stefhano"');  //TIPO STRING
+console.log(`'diuliano' "stefhano"`);
+
+console.log(123456789);    //TIPO NUMERICO
+
+console.log(25, 3.14,'diuliano'); //VARIAS DECLARAÇOES DIFERENTES DENTRO DO CONSOLE.LOG
+
