5a4224f6fc00d01798577704fd2ce4aa53992bca
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-05-27 19:23:07 -0300

    comentarios no codigo

---
+++ aula5/comentarios.js
@@ -0,0 +1,14 @@
+// serve para comentar trechos do codigo
+console.log('linha 1');
+//console.log('linha 2'); //da para fazer alteraçoes no codigo
+console.log('linha 3');
+console.log('linha 4');
+// comentários sao ignorados durante a leitura do codigo
+
+/*
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+
+*/
