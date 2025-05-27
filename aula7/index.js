1b094f1c6eea7c816cf21869282fec5ae00d49c5
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-05-27 19:24:33 -0300

    Variáveis let

---
+++ aula7/index.js
@@ -0,0 +1,16 @@
+// nao podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+// nao pode começar o nome de uma variavel com um numero
+// nao podem conter espaços ou traços
+// utilizamos camelCase
+// Case-Srnsitive
+// nao podemos redeclarar variaveis com let
+// nao utilize var, utilize let
+
+let nomeCompletoDoCliente= 'Diuliano S m m';
+
+let nome= 'Diuliano';
+console.log(nome,' nasceu em 2000.');
+console.log('Em 2024',nome, 'conheceu Jessica');
+console.log(nome,'casou-se com Jessica em 2026');
+console.log(nomeCompletoDoCliente);
