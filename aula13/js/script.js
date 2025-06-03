a3cb523c3adaf004573b2c4dec3b1df1bb6860bd
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-06-03 17:05:03 -0300

    estudos de javascript
    manipulação de window, prompt, alert e confirm no console do chrome

---
+++ aula13/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite um numero');
+num1= parseFloat(num1);
+num2= parseFloat(num2);
+const resultado = num1 + num2;
+alert('o resultado é: '+ resultado);
