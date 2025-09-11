b377b197c7b72537f9b9b00aefe8b1745631478b
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-09-11 17:39:55 -0300

    Array in JavaScript

---
+++ aula 20/index.js
@@ -0,0 +1,25 @@
+//let alunos = ['diuliano','joao', 'marcos'];
+//console.log(typeof alunos);
+//console.log(alunos instanceof Array);
+
+//console.log(alunos[1]);
+///alunos[0]= 'Eduardo';
+//alunos[3] = 'kaio';
+//console.log(alunos);
+//alunos.push('diuliano');
+//alunos.push('joao');
+//alunos.push('jessica');
+//delete alunos [1];
+//console.log(alunos[1]);
+//alunos.push('luiza');
+//alunos.push('eduardo');
+//console.log(alunos.slice(0, -2))
+//const remove=alunos.shift();
+//console.log(remove);
+//console.log(alunos);
+
+const array = [1,2,3,4,5];
+
+array[2] = 1024;
+array.push(2048);
+console.log(array);
