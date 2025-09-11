2f64ac829541182805b507ce31379e5c63b7920a
Dev_Diuliano <108770371+Stefhan55@users.noreply.github.com>
2025-09-11 17:40:46 -0300

    Array and functions in JavaScript

---
+++ aula 21/index.js
@@ -0,0 +1,27 @@
+//function saudacao(nome) {
+
+//return `Bom dia ${nome}!`;
+//}
+
+
+//const variavel = saudacao('Diuliano');
+//console.log(variavel);
+//function soma(x= 1, y= 1){
+
+ //   const resultado = x + y;
+ //   //console.log('ola mundo');
+ //   return resultado;
+     
+//}
+//console.log(soma(4,4));
+//console.log(soma());
+
+//const raiz = function(n){
+//return n ** 0.5;
+
+//};
+
+const raiz = n => n ** 0.5;
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
