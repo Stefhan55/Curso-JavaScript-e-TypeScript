6194a858032a924fa6e6ff4bba0c0e82dde8bf73
DevID <108770371+Stefhan55@users.noreply.github.com>
2025-09-15 14:32:30 -0300

    Logic Operator

---
+++ aula 26/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& -> AND -> E  todas as expressoes precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NAO
+*/
+//const expressaoAnd = true&& true&&true&& true;
+//const expressaoOr = true || false;
+//console.log(expressaoOr);
+//const usuario= 'diuliano';
+//const senha= '12345';
+//const vaiLogar = usuario === 'diuliano'&&senha === '12345';
+//console.log(vaiLogar);
+console.log(!Strue);
