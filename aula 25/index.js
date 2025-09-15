3bfa0eb24b87e514e6a3269ee2f9fcebccb4f034
DevID <108770371+Stefhan55@users.noreply.github.com>
2025-09-15 14:03:08 -0300

    Operators of comparation

---
+++ aula 25/index.js
@@ -0,0 +1,10 @@
+//Operadores de comparação
+/*=== Igualdade Estrita
+!= diferente
+!== diferente Estrito
+*/
+const num1 = 10;
+const num2 = '10'; 
+//const comp = num1 === num2;
+const comp = num1 !== num2;
+console.log(comp);
