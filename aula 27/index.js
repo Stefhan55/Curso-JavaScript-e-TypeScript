360b5cae631e299fa34290628a8e9c4a71b81cfb
DevID <108770371+Stefhan55@users.noreply.github.com>
2025-09-15 15:16:49 -0300

    Operators Logics

---
+++ aula 27/index.js
@@ -0,0 +1,19 @@
+/*&& ->  false && true -> false 'o valor mesmo'
+|| -> true && false -> vai retornar 'o valor verdadeiro'
+Falsy
+*false
+0
+''""``
+null / undefined
+NaN
+*/
+//function falaoi(){
+//    return 'oi';
+//}
+//let vaiExercutar = 'Diuliano'; 
+
+//console.log(vaiExercutar && falaoi());
+//console.log(0|| false || null|| 'Diuliano')
+//const corUsuario = null;
+//const corPadrao = corUsuario || 'preto';
+//console.log(corPadrao);
